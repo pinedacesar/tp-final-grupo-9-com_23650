f521ea08b918388a38740d89a2bedb8e02f9b76a
pinedacesar <cesar.pineda.assert@gmail.com>
2023-11-14 21:21:42 -0300

    Primer commit

---
+++ src/test/java/com/tpfinalgrupo9spring/TpFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpfinalgrupo9spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
