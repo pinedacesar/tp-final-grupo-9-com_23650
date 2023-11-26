5e8ce072cb3b632211c42f6fc1f55b21b7ef18ba
Pablo Tripodi <110062175+scorpius-t@users.noreply.github.com>
2023-11-26 11:40:40 -0300

    TODO: prueba de conexion: Terminar de configurar AccountService,crear controlador Despues crear dto, mapper y servicios de accounts

--- src/main/java/com/tpfinalgrupo9spring/services/AccountService.java
+++ src/main/java/com/tpfinalgrupo9spring/services/AccountService.java
@@ -1,25 +1,33 @@
 package com.tpfinalgrupo9spring.services;
 
+import com.tpfinalgrupo9spring.entities.AccounType;
 import com.tpfinalgrupo9spring.entities.Accounts;
 import com.tpfinalgrupo9spring.entities.UserEntity;
+import com.tpfinalgrupo9spring.repositories.AccountRepository;
+import org.springframework.stereotype.Service;
 
-//@Service
+@Service
 public class AccountService {
 
-    // public final AccountRepository accountRepository;
+     public final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
     // public final UserRepository userRepository;
 
-    public String create(Long owner, String tipo){
+    public String create(Long owner, AccounType tipo, String nombre){
         Accounts account =new Accounts();
 
         UserEntity user =new UserEntity();
         // busqueda usuario x id en DB
-        account.setNombre(tipo);
-        account.setOwner(user.getId());
+        account.setName(nombre);
+//        account.setOwner(user.getId());
+        account.setTipo(tipo);
         account.setSucursal("111");
         //cambiar dni x cuil
         String numCuenta="001"; //contar en DB cantidad de owners con id del usr
-        account.setCbu(generarCBU(account.getSucursal(), account.getNombre(),user.getDni(),numCuenta));
+        account.setCbu(generarCBU(account.getSucursal(), account.getName(),user.getDni(),numCuenta));
 
         //accountRepository.save(account);
 
